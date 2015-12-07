0223be7a6bc108e3e1a9752b8f8d49d03644654a
Korko <jeremy.lemesle@korko.fr>
2015-12-07 11:36:06 +0100

    Missing space

--- resources/lang/fr/validation.php
+++ resources/lang/fr/validation.php
@@ -78,7 +78,7 @@ return [
     'url'                  => "Le format de l'URL de :attribute n'est pas valide.",
 
     'arrayunique'          => 'Le champ :attribute contient des valeurs similaires.',
-    'contains'             => 'Le champ:attribute doit contenir les valeurs suivantes : :values.',
+    'contains'             => 'Le champ :attribute doit contenir les valeurs suivantes : :values.',
     'fieldin'              => 'Le champ :attribute contient des valeurs qui ne correspondent pas aux valeurs du champ :other.',
 
     /*
