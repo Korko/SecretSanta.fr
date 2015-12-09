fd8cccd06fcc8638511b9e4524667f4c5f82ef20
Korko <jeremy.lemesle@korko.fr>
2015-12-09 21:25:21 +0100

    Change design + some fixes

--- resources/views/randomForm.blade.php
+++ resources/views/randomForm.blade.php
@@ -3,7 +3,7 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="no-js"> <!--<![endif]-->
+<html class="no-js" lang="fr"> <!--<![endif]-->
 <head>
     <title>Secret Santa</title>
 
@@ -11,35 +11,26 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="Outils pour aider à organiser un secret-santa">
 
+    <!-- facebook image -->
     <link rel="image_src" href="media/img/logo_black.png" />
 
     <!-- css -->
-    <link rel="stylesheet" href="media/css/bootstrap.min.css">
-    <link rel="stylesheet" href="media/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="media/css/font-awesome.min.css">
-    <link rel="stylesheet" href="media/css/main.css">
-
-    <!-- google font -->
-    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Kreon:300,400,700'>
-
-    <!-- js -->
-    <script src="media/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-
-    <style>
-        .placeholder {display:none;}
-        .participant .participant-remove-wrapper {width:179px;}
-        .participant:nth-child(-n+2) .participant-remove {display:none;}
-
-        #mailContent { max-width: 100%; }
-
-        @-moz-document url-prefix() {
-          fieldset { display: table-cell; }
-        }
-    </style>
+    <link rel="stylesheet" href="media/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="media/css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" href="media/css/font-awesome.min.css" />
     <link rel="stylesheet" href="media/css/alertify.core.css" />
     <link rel="stylesheet" href="media/css/alertify.default.css" />
     <link rel="stylesheet" href="media/css/alertify.bootstrap.css" />
+    <link rel="stylesheet" href="media/css/main.css" />
+
+    <!-- google font -->
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Kreon:300,400,700' />
+
+    <!-- js -->
+    <script src="media/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body data-spy="scroll" data-target="#navbar" data-offset="120" >
     <!--[if lt IE 7]>
@@ -55,11 +46,10 @@
                 </a>
             </div>
             <ul class="nav navbar-nav hidden-xs">
-                <li><a href="#what">Quoi</a></li>
-                <li><a href="#how">Comment</a></li>
+                <li><a href="#what">Qu'est-ce que c'est ?</a></li>
+                <li><a href="#how">Comment faire ?</a></li>
 
-
-                <li><a href="#form">Allez c'est parti</a></li>
+                <li><a href="#form">Allez, c'est parti !</a></li>
 
                 <!--fix for scroll spy active menu element-->
                 <li style="display:none;"><a href="#header"></a></li>
@@ -74,7 +64,7 @@
             <div class="banner">
                 <h1 class="">Secret Santa</h1>
             </div>
-            <div class="subtitle"><h4>Offrez-vous des cadeaux... secrêtement !</h4></div>
+            <div class="subtitle"><h4>Offrez-vous des cadeaux... secrètement !</h4></div>
         </div>
         <div class="bottom text-center">
             <a id="scrollDownArrow" href="#"><i class="fa fa-chevron-down"></i></a>
@@ -88,9 +78,22 @@
             <h2 class="section-title text-center">Qu'est-ce que c'est ?</h2>
             <p class="lead main text-center">Description du Secret Santa</p>
             <div class="row text-center what">
-
-
-
+                <ul class="media-list">
+                    <li class="media">
+                        <div class="media-left media-middle">
+                            <img class="media-object" src="media/img/calendar-icon.png" />
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Le principe</h4>
+                            <p>
+                                Secret Santa est un moyen drôle et original de s'offrir anonymement des cadeaux entre amis, collègues...<br />
+                                Le déroulement est simple : chaque participant reçoit, de façon aléatoire, le nom de la personne à qui il devra faire un cadeau.<br />
+                                Le montant du cadeau est généralement fixé au préalable (2€, 5€, 10€...)<br />
+                                Le but n'est pas forcément de faire un beau cadeau mais de faire original. Soyez créatifs !
+                            </p>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
         <!-- /.container -->
@@ -110,8 +113,36 @@
         <div class="container inner">
             <h2 class="section-title text-center">Comment faire ?</h2>
             <p class="lead main text-center">Vous allez voir, c'est simple !</p>
-            <div class="row text-center what">
-
+            <div class="row text-center how">
+                <ul class="media-list">
+                    <li class="media">
+                        <div class="media-left media-middle">
+                            <img class="media-object" src="media/img/user-icon.png" />
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Première étape : remplir les informations des participants</h4>
+                            <p>
+                                Pour chaque personne, il faut indiquer un nom/prénom ou un pseudonyme. Deux participants ne peuvent avoir le même nom, sinon il est impossible de les différencier.<br />
+                                Grâce aux boutons "Ajouter un participant" et "Enlever un participant", ajustez le nombre de personnes.<br />
+                                (Optionel) Ajoutez des exclusions. Si vous ne voulez pas que les conjoints puissent se piocher l'un l'autre, remplissez le champ "Partenaire".
+                            </p>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <div class="media-body">
+                            <h4 class="media-heading">Seconde étape : préparer le mail</h4>
+                            <p>
+                            Il faut spécifier le titre ainsi que le corps du message que chaque participant recevra.<br />
+                            Le mot clef "{TARGET}" est obligatoire dans le corps du mail afin de donner à chaque personne sa "cible".<br />
+                            (Optionel) Vous pouvez aussi utiliser le mot clef "{SANTA}" qui sera lui remplacé par le nom du destinataire du mail.
+                            </p>
+                        </div>
+                        <div class="media-right media-middle">
+                            <img class="media-object" src="media/img/mail-icon.png" />
+                        </div>
+                    </li>
+                </ul>
+                <div class="well well-lg">NB : Aucune donnée n'est sauvegardée ou utilisée à d'autres fins que pour ce script. Le code source est disponible sur <a href="https://github.com/Korko/SecretSanta">GitHub</a></div>
             </div>
         </div>
         <!-- /.container -->
@@ -187,13 +218,15 @@
 
                         <div class="form-group">
                             <label for="mailTitle">Titre du mail</label>
-                            <input id="mailTitle" type="text" name="title" required="required" placeholder="exemple : Soirée secretsanta du 23 décembre" value="{{ old('title') }}" class="form-control" />
+                            <input id="mailTitle" type="text" name="title" required="required" placeholder="exemple : Soirée secretsanta du 23 décembre chez Martin" value="{{ old('title') }}" class="form-control" />
                         </div>
                         <div class="form-group">
                             <label for="mailContent">Contenu du mail</label>
-                            <textarea id="mailContent" name="content" required="required" placeholder="exemple : Salut {SANTA}, pour la soirée secret santa, ta cible c'est {TARGET}." class="form-control" rows="3">{{ old('content') }}</textarea>
+                            <textarea id="mailContent" name="content" required="required" placeholder="exemple : Salut {SANTA}, pour la soirée secret santa, ta cible c'est {TARGET}. Pour rappel, le montant du cadeau est de 3€ !" class="form-control" rows="3">{{ old('content') }}</textarea>
                             <p class="help-block">Utilisez "{SANTA}" pour le nom de celui qui recevra le mail et "{TARGET}" pour le nom de sa cible.</p>
+                            <p class="help-block">Conseil : Pensez à rappeler la date, le lieu ainsi que le montant du cadeau.</p>
                         </div>
+
                         <button type="submit" class="btn btn-default">Lancez l'aléatoire !</button>
                     </fieldset>
                 </form>
