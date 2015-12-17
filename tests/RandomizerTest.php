aeeb471ca7f8210d250b9067783ac385499184d9
Korko <jeremy.lemesle@korko.fr>
2015-12-17 10:47:44 -0500

    Applied fixes from StyleCI

--- tests/RandomizerTest.php
+++ tests/RandomizerTest.php
@@ -8,12 +8,12 @@ class RandomizerTest extends TestCase
     {
         $this->assertEquals([0 => 'B', 1 => 'A'], Randomizer::randomize([
         0 => ['name' => 'A', 'partner' => null],
-        1 => ['name' => 'B', 'partner' => null]
+        1 => ['name' => 'B', 'partner' => null],
     ]));
 
         $this->assertEquals([0 => 'A', 1 => 'B'], Randomizer::randomize([
         0 => ['name' => 'B', 'partner' => null],
-        1 => ['name' => 'A', 'partner' => null]
+        1 => ['name' => 'A', 'partner' => null],
     ]));
     }
 
@@ -22,13 +22,13 @@ class RandomizerTest extends TestCase
         $this->assertEquals([0 => 'B', 1 => 'C', 2 => 'A'], Randomizer::randomize([
                 0 => ['name' => 'A', 'partner' => null],
                 1 => ['name' => 'B', 'partner' => 'A'],
-        2 => ['name' => 'C', 'partner' => 'B']
+        2         => ['name' => 'C', 'partner' => 'B'],
         ]));
 
         $this->assertEquals([0 => 'C', 1 => 'A', 2 => 'B'], Randomizer::randomize([
                 0 => ['name' => 'A', 'partner' => 'B'],
                 1 => ['name' => 'B', 'partner' => 'C'],
-                2 => ['name' => 'C', 'partner' => null]
+                2 => ['name' => 'C', 'partner' => null],
         ]));
     }
 
@@ -41,7 +41,7 @@ class RandomizerTest extends TestCase
         $this->assertEquals([0 => 'C', 1 => 'A', 2 => 'B'], Randomizer::randomize([
                 0 => ['name' => 'A', 'partner' => 'B'],
                 1 => ['name' => 'B', 'partner' => 'A'],
-                2 => ['name' => 'C', 'partner' => null]
+                2 => ['name' => 'C', 'partner' => null],
         ]));
     }
 }
