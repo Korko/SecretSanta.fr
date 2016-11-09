fb86a03807c68e87d767495ec0d9ad56ab948cd8
Korko <jeremy.lemesle@korko.fr>
2016-11-09 19:34:48 +0100

    Remove Piwik from source code

--- resources/views/randomForm.blade.php
+++ resources/views/randomForm.blade.php
@@ -323,23 +323,5 @@
         alertify.alert("{{ session('message') }}");
     </script>
     @endif
-
-    <!-- Piwik -->
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-      _paq.push(["setDomains", ["*.secretsanta.fr"]]);
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//k0rko.fr/piwik/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', 3]);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
-    <noscript><p><img src="//k0rko.fr/piwik/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>
-    <!-- End Piwik Code -->
 </body>
 </html>
