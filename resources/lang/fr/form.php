ff5f3bf3803554ac2c298c41262990a995c0738d
Korko <jeremy.lemesle@korko.fr>
2016-12-18 18:33:30 +0100

    When form is sent, change the button to see it

--- resources/lang/fr/form.php
+++ resources/lang/fr/form.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'nav.what' => "Qu'est-ce que c'est ?",
+    'nav.what' => 'Qu\'est-ce que c\'est ?',
     'nav.how'  => 'Comment faire ?',
-    'nav.go'   => "Allez, c'est parti !",
+    'nav.go'   => 'Allez, c\'est parti !',
 
     'subtitle'   => 'Offrez-vous des cadeaux... secrètement !',
 
-    'section.what.title'    => "Qu'est-ce que c'est ?",
+    'section.what.title'    => 'Qu\'est-ce que c\'est ?',
     'section.what.subtitle' => 'Description du Secret Santa',
     'section.what.heading1' => 'Le principe',
     'section.what.content1' => "Secret Santa est un moyen drôle et original de s'offrir anonymement des cadeaux entre amis, collègues...
@@ -16,7 +16,7 @@ Le montant du cadeau est généralement fixé au préalable (2€, 5€, 10€..
 Le but n'est pas forcément de faire un beau cadeau mais d'être créatif !",
 
     'section.how.title'    => 'Comment faire ?',
-    'section.how.subtitle' => "Vous allez voir, c'est simple !",
+    'section.how.subtitle' => 'Vous allez voir, c\'est simple !',
     'section.how.heading1' => 'Première étape : spécifier le nombre et les noms des participants',
     'section.how.content1' => "Grâce aux boutons \"Ajouter un participant\" et \"Enlever un participant\", il est possible d'ajuster le nombre de personnes.
 Pour chaque personne, indiquez un nom/prénom ou un pseudonyme. Deux participants ne peuvent avoir le même nom, sinon il est impossible de les différencier.
@@ -29,10 +29,10 @@ Pour cela, précisez pour chacun une adresse e-mail et/ou un numéro de téléph
     'section.how.content3' => "Il ne vous reste plus qu'à remplir le titre et le corps du courriel ou du SMS que les participants recevront.
 Le mot clef \"{TARGET}\" est obligatoire dans le corps du message afin de donner à chaque personne sa \"cible\".
 (Optionel) Vous pouvez aussi utiliser le mot clef \"{SANTA}\" qui sera remplacé par le nom du destinataire du message.",
-    'section.how.notice' => "Pour votre information : secretsanta.fr ne sauvegarde aucune de vos données ni ne les utilise à d'autres fins. Le code source est disponible sur :link",
+    'section.how.notice' => 'Pour votre information : secretsanta.fr ne sauvegarde aucune de vos données ni ne les utilise à d\'autres fins. Le code source est disponible sur :link',
 
     'section.go.title'    => 'À vous de jouer !',
-    'section.go.subtitle' => "Remplissez, cliquez et c'est parti !",
+    'section.go.subtitle' => 'Remplissez, cliquez et c\'est parti !',
 
     'success' => 'Envoyé avec succès !',
 
@@ -55,7 +55,7 @@ Le mot clef \"{TARGET}\" est obligatoire dans le corps du message afin de donner
     'mail.title.placeholder' => 'ex : Soirée secretsanta du 23 décembre chez Martin',
 
     'mail.content'             => 'Contenu du mail',
-    'mail.content.placeholder' => "ex : Salut {SANTA}, pour la soirée secret santa, ta cible c'est {TARGET}. Pour rappel, le montant du cadeau est de 3€ !",
+    'mail.content.placeholder' => 'ex : Salut {SANTA}, pour la soirée secret santa, ta cible c\'est {TARGET}. Pour rappel, le montant du cadeau est de 3€ !',
     'mail.post'                => '----
 via SecretSanta.fr',
     'mail.content.tip1' => 'Utilisez "{SANTA}" pour le nom de celui qui recevra le mail et "{TARGET}" pour le nom de sa cible.',
@@ -65,11 +65,12 @@ via SecretSanta.fr',
 
     'sms.content'             => 'Contenu du sms :span (:left caractères restants) :espan',
     'sms.content.multiple'    => 'Contenu des :count sms :span (:left caractères restants) :espan',
-    'sms.content.placeholder' => "ex : Salut {SANTA}, pour la soirée secret santa du 23 décembre chez Martin, ta cible c'est {TARGET}. Pour rappel, le montant du cadeau est de 3€ !",
+    'sms.content.placeholder' => 'ex : Salut {SANTA}, pour la soirée secret santa du 23 décembre chez Martin, ta cible c\'est {TARGET}. Pour rappel, le montant du cadeau est de 3€ !',
     'sms.post'                => '- via SecretSanta.fr',
     'sms.content.tip1'        => 'Utilisez "{SANTA}" pour le nom de celui qui recevra le sms et "{TARGET}" pour le nom de sa cible.',
     'sms.content.tip2'        => 'Conseil : Pensez à rappeler la date, le lieu ainsi que le montant du cadeau.',
 
-    'submit'  => "Lancez l'aléatoire !",
+    'submit'  => 'Lancez l\'aléatoire !',
     'sending' => 'Calcul en cours',
+    'sent'    => 'Envoyé',
 ];
