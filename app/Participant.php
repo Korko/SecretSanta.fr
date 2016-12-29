82df8ffc23a9f28cd1556ae0dd53a6a1ef50afa8
Korko <jeremy.lemesle@korko.fr>
2016-12-29 17:59:50 +0100

    Add draw and participant in database

---
+++ app/Participant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Korko\SecretSanta;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Participant extends Model
+{
+    public $timestamps = false;
+}
---
+++ database/migrations/2016_12_27_140635_create_participants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('draw_id');
+            $table->text('iv');
+            $table->text('email');
+            $table->text('challenge');
+
+            $table->foreign('draw_id')->references('id')->on('draws');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
