c1b24b73267cce99197ee6088c7902220144b7bd
Korko <jeremy.lemesle@korko.fr>
2017-01-03 22:28:15 +0100

    Correctly get iv and challenge

--- app/Http/Controllers/DearSantaController.php
+++ app/Http/Controllers/DearSantaController.php
@@ -9,9 +9,12 @@ class DearSantaController extends Controller
 {
     public function view(Participant $santa)
     {
+        // Times 2 because we are in UTF8 and the iv is in hexadecimal
+        $ivLength = openssl_cipher_iv_length('aes256') * 2;
+
         return view('dearSanta', [
-            'challenge' => $santa->challenge,
-            'iv' => bin2hex($santa->iv)
+            'challenge' => substr($santa->challenge, $ivLength),
+            'iv' => substr($santa->challenge, 0, $ivLength)
         ]);
     }
 }
