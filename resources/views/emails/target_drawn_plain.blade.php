63880f1891a12671e3c6cfe608db32adc44544b0
Korko <jeremy.lemesle@korko.fr>
2017-03-07 15:26:06 +0100

    Reformat emails

--- resources/views/emails/target_drawn_plain.blade.php
+++ resources/views/emails/target_drawn_plain.blade.php
@@ -1,3 +1,4 @@
 {{ str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $content) }}
+
 {{ !empty($dearSantaLink) ? trans('form.mail.post2', ['link' => $dearSantaLink]) : trans('form.mail.post') }}
 
