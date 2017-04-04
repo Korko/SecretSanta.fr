4e67fd93089709593fb7eae99846ed74fe71cdb0
Korko <jeremy.lemesle@korko.fr>
2017-04-04 23:12:12 +0200

    Update headers.php

--- resources/lang/fr/headers.php
+++ resources/lang/fr/headers.php
@@ -3,5 +3,5 @@
 return [
     'title'       => 'SecretSanta.fr - Offrez-vous des cadeaux... secrètement !',
     'description' => 'SecretSanta.fr est un outil simple, gratuit et sans inscription pour aider à organiser une remise de cadeau en secret entre amis ou collègues (appelée secret santa).',
-    'keywords'    => 'soiree,organisation,secretsanta,noel,cadeau,secret,gratuit,simple,inscription,libre,santa,secret-santa,collegues,amis',
+    'keywords'    => 'soiree,organisation,secretsanta,noel,cadeau,secret,gratuit,simple,inscription,libre,santa,secret-santa,collegues,amis,anonyme,aléatoire,random',
 ];
