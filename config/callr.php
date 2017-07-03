eaa5a63d6beda1f5407f509b51a768ef8d961149
Korko <jeremy.lemesle@korko.fr>
2017-07-03 11:19:35 +0200

    Change Twilio to Callr

---
+++ config/callr.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'username' => env('CALLR_USERNAME', 'username'),
+	'password' => env('CALLR_PASSWORD', 'password'),
+	'alias' => env('CALLR_ALIAS', ''),
+        'sender' => env('CALLR_SENDER', ''),
+];
