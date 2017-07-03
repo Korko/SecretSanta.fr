e83e733cd7532852c43a8e8b294011fdf7f21a81
Korko <jeremy.lemesle@korko.fr>
2017-07-03 14:12:52 +0200

    Fixes SMS (need +33 form instead of 0)

--- tests/Unit/RequestTest.php
+++ tests/Unit/RequestTest.php
@@ -77,12 +77,12 @@ class RequestTest extends RequestCase
 
         Sms::shouldReceive('message')
             ->once()
-            ->with('0612345678', '#test sms "tata\' => &tutu#')
+            ->with('+33612345678', '#test sms "tata\' => &tutu#')
             ->andReturn(true);
 
         Sms::shouldReceive('message')
             ->once()
-            ->with('0712345678', '#test sms "tutu\' => &toto#')
+            ->with('+33712345678', '#test sms "tutu\' => &toto#')
             ->andReturn(true);
 
         $this->assertEquals(0, Draw::count());
