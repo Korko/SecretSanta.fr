3eb211176df76dc0a1ccd1bceb8dfc58721854ac
Korko <jeremy.lemesle@korko.fr>
2017-11-08 17:51:47 +0100

    Revert to a classic recaptcha

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -2,7 +2,3 @@ var alertify = require('alertify.js');
 if(window.global.alert) {
     alertify.alert(window.global.alert);
 }
-
-window.submitForm = function() {
-    jQuery('#randomForm .submitform').click();
-};
