d5fa07681245147a43437b28d5f3af7b71d1805f
Korko <jeremy.lemesle@korko.fr>
2017-11-09 23:20:38 +0100

    Fix new lines in html email not kept

--- resources/views/emails/target_drawn.blade.php
+++ resources/views/emails/target_drawn.blade.php
@@ -1,4 +1,4 @@
-<p>{{ str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $content) }}</p>
+<p>{!! nl2br(htmlentities( str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $content) )) !!}</p>
 
 {!! nl2br(htmlentities( !empty($dearSantaLink) ? trans('form.mail.post2', ['link' => $dearSantaLink]) : trans('form.mail.post') )) !!}
 
