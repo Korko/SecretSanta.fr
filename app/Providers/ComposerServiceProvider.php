6d9e06581610f4d8190b5adfc900ee119791f994
Korko <jeremy.lemesle@korko.fr>
2017-12-06 15:21:52 +0100

    Add current tag in fork me ribbon

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Korko\SecretSanta\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use View;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('templates/layout', function($view)
+        {
+            $view->with('version', exec('git describe --tags --abbrev=0'))
+                 ->with('version_date', exec('git log -1 --format=%ai'));
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
