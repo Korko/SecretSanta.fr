e85dc57dbe8da7f86b5e0898aaf7cff17acd6761
Korko <jeremy.lemesle@korko.fr>
2018-04-27 15:31:57 +0200

    Use hashids instead of raw ids

---
+++ app/Http/Middleware/Unhash.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Korko\SecretSanta\Http\Middleware;
+
+use Closure;
+use Hashids;
+use Illuminate\Foundation\Http\Middleware\TransformsRequest as Middleware;
+
+class Unhash extends Middleware
+{
+    /**
+     * Transform the given value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function transform($key, $value)
+    {
+        if (! array_key_exists($key, config('hashids.parameters'))) {
+            return $value;
+        }
+
+        $connection = config('hashids.parameters')[$key];
+        return array_get(Hashids::connection($connection)->decode($value), 0, $value);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@
 Route::get('/', 'RandomFormController@view');
 Route::post('/', 'RandomFormController@handle');
 
+Route::pattern('participant', '[0-9a-zA-Z]{'.config('hashids.connections')[config('hashids.default')]['length'].'}');
 Route::get('/dearsanta/{participant}', 'DearSantaController@view')->name('dearsanta');
 Route::post('/dearsanta/{participant}', 'DearSantaController@handle');
 
 if ($this->app->environment('local', 'dev', 'testing')) {
-    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+    Route::get('/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 }
