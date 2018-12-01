21f2e24d5562effef26445010f7de7d51a2583bc
Laravel Shift <shift@laravelshift.com>
2018-12-01 18:23:36 +0000

    Shift cleanup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\TrustProxies::class,
+        \Korko\SecretSanta\Http\Middleware\TrustProxies::class,
     ];
 
     /**
