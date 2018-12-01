1f9f134bd66db3e957c29b0e12306901ed4b3e1b
Korko <jeremy.lemesle@korko.fr>
2018-12-02 00:09:18 +0100

    Make some changes for laravel upgrades

--- tests/Feature/RequestCase.php
+++ tests/Feature/RequestCase.php
@@ -8,8 +8,11 @@ class RequestCase extends TestCase
 {
     public function ajaxPost($url, $postArgs, $code = 200)
     {
-        $server = ['HTTP_X-Requested-With' => 'XMLHttpRequest'];
-        $response = $this->call('POST', $url, $postArgs, [], [], $server);
+        $headers = [
+            'Accept'           => 'application/json',
+            'X-Requested-With' => 'XMLHttpRequest'
+        ];
+        $response = $this->post($url, $postArgs, $headers);
         $this->assertEquals($code, $response->status(), $response->getContent());
 
         return json_decode($response->content(), true);
