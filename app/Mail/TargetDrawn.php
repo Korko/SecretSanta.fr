cb8900085fbe8f277fb86a403b9993908c5a6a57
Korko <jeremy.lemesle@korko.fr>
2018-12-04 14:03:16 +0100

    Rework sendMail/sendSMS + add parameters to TargetDrawn

--- app/Mail/TargetDrawn.php
+++ app/Mail/TargetDrawn.php
@@ -5,10 +5,11 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Sichikawa\LaravelSendgridDriver\SendGrid;
 
 class TargetDrawn extends Mailable
 {
-    use Queueable, SerializesModels;
+    use Queueable, SerializesModels, SendGrid;
 
     public $santa;
     public $target;
@@ -16,20 +17,26 @@ class TargetDrawn extends Mailable
     public $subject;
     public $content;
 
+    public $organizer;
+
     public $dearSantaLink;
+
+    public $customArgs;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($santa, $target, $subject, $content, $dearSantaLink = null)
+    public function __construct($santa, $target, $subject, $content, $organizer, $dearSantaLink = null, $customArgs = [])
     {
         $this->santa = $santa;
         $this->target = $target;
         $this->subject = $subject;
         $this->content = $content;
+        $this->organizer = $organizer;
         $this->dearSantaLink = $dearSantaLink;
+        $this->customArgs = $customArgs;
     }
 
     /**
@@ -41,6 +48,19 @@ class TargetDrawn extends Mailable
     {
         return $this->subject($this->subject)
                     ->view('emails.target_drawn')
-                    ->text('emails.target_drawn_plain');
+                    ->text('emails.target_drawn_plain')
+                    ->sendgrid([
+                        'custom_args' => [
+                            'data' => json_encode($this->customArgs + [
+                                'organizer'     => $this->organizer,
+                                'santa'         => $this->santa,
+                                'target'        => $this->target,
+                                'subject'       => $this->subject,
+                                'content'       => $this->content,
+                                'dearSantaLink' => $this->dearSantaLink,
+                            ]),
+                        ],
+                    ]);
+
     }
 }
