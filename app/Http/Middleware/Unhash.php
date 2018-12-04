5ca7a5003a4f2ee901a4d9b81d06d3fbf354b0ab
Korko <jeremy.lemesle@korko.fr>
2018-12-04 14:08:22 +0100

    Apply fixes from StyleCI

--- app/Http/Middleware/Unhash.php
+++ app/Http/Middleware/Unhash.php
@@ -2,7 +2,6 @@
 
 namespace Korko\SecretSanta\Http\Middleware;
 
-use Closure;
 use Hashids;
 use Illuminate\Foundation\Http\Middleware\TransformsRequest as Middleware;
 
@@ -11,17 +10,19 @@ class Unhash extends Middleware
     /**
      * Transform the given value.
      *
-     * @param  string  $key
-     * @param  mixed  $value
+     * @param string $key
+     * @param mixed  $value
+     *
      * @return mixed
      */
     protected function transform($key, $value)
     {
-        if (! array_key_exists($key, config('hashids.parameters'))) {
+        if (!array_key_exists($key, config('hashids.parameters'))) {
             return $value;
         }
 
         $connection = config('hashids.parameters')[$key];
+
         return array_get(Hashids::connection($connection)->decode($value), 0, $value);
     }
 }
