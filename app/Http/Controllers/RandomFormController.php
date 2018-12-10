0d90f7327faf7eb878cc8b8c914a64562bd855da
Korko <jeremy.lemesle@korko.fr>
2018-12-10 23:44:55 +0100

    Split controllers and switch to base64 instead of hex

--- app/Http/Controllers/RandomFormController.php
+++ app/Http/Controllers/RandomFormController.php
@@ -2,42 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Draw;
 use App\Exceptions\SolverException;
 use App\Http\Requests\RandomFormRequest;
-use App\Mail\TargetDrawn;
-use App\Participant;
-use App\Services\AsymmetricalPublicEncrypter as AsymmetricalEncrypter;
-use App\Services\SymmetricalEncrypter;
+use App\Services\DrawHandler;
 use Facades\App\Services\HatSolver as Solver;
-use Facades\App\Services\SmsTools as SmsTools;
-use Hashids;
 use Illuminate\Http\Request;
-use Mail;
 use Metrics;
-use Sms;
 
 class RandomFormController extends Controller
 {
-    private $symKey;
-    private $encrypter;
-
-    private $asymKeys;
-    private $aEncrypter;
-
-    public function __construct()
-    {
-        $this->symKey = SymmetricalEncrypter::generateKey();
-        $this->encrypter = new SymmetricalEncrypter($this->symKey);
-
-        $this->asymKeys = AsymmetricalEncrypter::generateKeys();
-        $this->aEncrypter = new AsymmetricalEncrypter($this->asymKeys['public']);
-    }
-
     public function handle(RandomFormRequest $request)
     {
         try {
-            $this->draw($request);
+            $this->drawAndInform($request);
 
             $message = trans('message.sent');
 
@@ -53,22 +30,29 @@ class RandomFormController extends Controller
         }
     }
 
-    protected function draw(Request $request)
+    protected function drawAndInform(Request $request)
     {
         $participants = $this->getParticipants($request);
-
         $hat = Solver::one($participants, array_column($participants, 'exclusions'));
 
         Metrics::increment('draws');
         Metrics::increment('participants', count($participants));
 
-        if ($request->input('contentMail')) {
-            $this->sendMails($request, $participants, $hat);
+        $dearSantaExpiration = null;
+        if ($request->input('dearsanta')) {
+            $dearSantaExpiration = $request->input('dearsanta-expiration');
         }
 
-        if ($request->input('contentSMS')) {
-            $this->sendSMSs($request, $participants, $hat);
-        }
+        $mailContent = [
+            'title' => $request->input('title'),
+            'body' => $request->input('contentMail'),
+        ];
+
+        $smsContent = [
+            'body' => $request->input('contentSMS'),
+        ];
+
+        return (new DrawHandler())->contactParticipants($participants, $hat, $mailContent, $smsContent, $dearSantaExpiration);
     }
 
     protected function getParticipants(Request $request)
@@ -92,97 +76,5 @@ class RandomFormController extends Controller
         }
 
         return $participants;
-    }
-
-    protected function sendMails(Request $request, array $participants, array $hat)
-    {
-        $title = $request->input('title');
-        $body = $request->input('contentMail');
-        $dearSantaExpiration = $request->input('dearsanta-expiration');
-
-        $organizer = $participants[0];
-
-        // Use symmetrical encryption, we don't need to hide it from each other
-        $this->draw = new Draw();
-        $this->draw->expiration = $dearSantaExpiration;
-        $this->draw->title = $this->encrypter->encrypt($title);
-        $this->draw->body = $this->encrypter->encrypt($body);
-        $this->draw->organizer_name = $this->encrypter->encrypt($organizer['name']);
-        $this->draw->organizer_email = $this->encrypter->encrypt($organizer['email']);
-        $this->draw->save();
-
-        $personalizations = [];
-
-        foreach ($hat as $santaIdx => $targetIdx) {
-            $santa = $participants[$santaIdx];
-            $target = $participants[$targetIdx];
-
-            if (!empty($santa['email'])) {
-                // Santa of that santa
-                $superSanta = $participants[array_search($santaIdx, $hat)];
-
-                $dearSantaLink = null;
-                if ($request->input('dearsanta')) {
-                    $dearSantaLink = $this->getDearSantaLink($superSanta, $dearSantaExpiration);
-                }
-
-                $personalizations[] = [
-                    'to' => [
-                        'email' => $santa['email'],
-                        'name'  => $santa['name'],
-                    ],
-                    'substitutions' => [
-                        '{SANTA}'  => $santa['name'],
-                        '{TARGET}' => $target['name'],
-                        ':link'    => $dearSantaLink,
-                    ],
-                ];
-            }
-        }
-
-        if (!empty($personalizations)) {
-            Metrics::increment('email', count($personalizations));
-
-            Mail::to($organizer['email'])
-                ->send(new TargetDrawn($title, $body, $personalizations));
-        }
-    }
-
-    protected function sendSMSs(Request $request, array $participants, array $hat)
-    {
-        foreach ($hat as $santaIdx => $targetIdx) {
-            $santa = $participants[$santaIdx];
-            $target = $participants[$targetIdx];
-
-            if (!empty($santa['phone'])) {
-                Metrics::increment('phone');
-                Metrics::increment('sms', SmsTools::count($request->input('contentSMS')));
-
-                $contentSms = str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $request->input('contentSMS'));
-                Sms::message($santa['phone'], $contentSms);
-            }
-        }
-    }
-
-    protected function getDearSantaLink(array $santa, $expirationDate)
-    {
-        $participant = $this->addParticipant($santa, $expirationDate);
-
-        return route('dearsanta', ['santa' => Hashids::encode($participant->id)]).'#'.bin2hex($this->asymKeys['private']);
-    }
-
-    private function addParticipant(array $santa, $expirationDate)
-    {
-        // Use Asymmetrical encrypter, only the reciptient should be able to decrypt!
-        $participant = new Participant();
-        $participant->draw_id = $this->draw->id;
-        $participant->santa_name = $this->aEncrypter->encrypt($santa['name']);
-        $participant->santa_email = $this->aEncrypter->encrypt($santa['email']);
-        $participant->challenge = $this->aEncrypter->encrypt(Participant::CHALLENGE, false); // tested by JS so no serializing
-        $participant->public_key = $this->asymKeys['public'];
-
-        $participant->save();
-
-        return $participant;
     }
 }
---
+++ app/Services/DrawHandler.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Services;
+
+use App\Draw;
+use App\Mail\TargetDrawn;
+use App\Participant;
+use App\Services\AsymmetricalEncrypter;
+use App\Services\SymmetricalEncrypter;
+use Facades\App\Services\SmsTools as SmsTools;
+use Hashids;
+use Mail;
+use Metrics;
+use Sms;
+
+class DrawHandler
+{
+    private $symKey;
+    private $asymKeys;
+
+    public function __construct()
+    {
+        $this->symKey = SymmetricalEncrypter::generateKey(config('app.cipher'));
+        $this->asymKeys = AsymmetricalEncrypter::generateKeys(1024);
+    }
+
+    public function contactParticipants(array $participants, array $hat, array $mailContent, array $smsContent, $dearSantaExpiration = null)
+    {
+        if (!empty($mailContent)) {
+            $this->sendMails($participants, $hat, $mailContent, $dearSantaExpiration);
+        }
+
+        if (!empty($smsContent)) {
+            $this->sendSMSs($participants, $hat, $smsContent['body']);
+        }
+    }
+
+    protected function sendMails(array $participants, array $hat, array $mailContent, $dearSantaExpiration = null)
+    {
+        $this->draw = Draw::prepareAndSave($mailContent, $dearSantaExpiration, $participants[0], $this->symKey);
+
+        foreach ($hat as $santaIdx => $targetIdx) {
+            $santa = $participants[$santaIdx];
+            $target = $participants[$targetIdx];
+
+            if (!empty($santa['email'])) {
+                // Santa of that santa
+                $superSanta = $participants[array_search($santaIdx, $hat)];
+
+                $dearSantaLink = null;
+                if ($dearSantaExpiration !== null) {
+                    $dearSantaLink = $this->getDearSantaLink($superSanta);
+                }
+
+                $this->sendSingleMail($mailContent, $santa, $target, $dearSantaLink);
+            }
+        }
+    }
+
+    protected function sendSingleMail(array $mailContent, array $santa, array $target, $dearSantaLink = null)
+    {
+        $substitutions = [
+            '{SANTA}'  => $santa['name'],
+            '{TARGET}' => $target['name'],
+            ':link'    => $dearSantaLink,
+        ];
+
+        Metrics::increment('email');
+
+        Mail::to($santa['email'], $santa['name'])
+            ->send(new TargetDrawn($mailContent['title'], $mailContent['body'], ['substitutions' => $substitutions]));
+    }
+
+    protected function sendSMSs(array $participants, array $hat, $smsBody)
+    {
+        foreach ($hat as $santaIdx => $targetIdx) {
+            $santa = $participants[$santaIdx];
+            $target = $participants[$targetIdx];
+
+            if (!empty($santa['phone'])) {
+                $this->sendSingleSms($smsBody, $santa, $target);
+            }
+        }
+    }
+
+    protected function sendSingleSms($smsBody, array $santa, array $target)
+    {
+        Metrics::increment('phone');
+        Metrics::increment('sms', SmsTools::count($smsBody));
+
+        $contentSms = str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $smsBody);
+        Sms::message($santa['phone'], $contentSms);
+    }
+
+    protected function getDearSantaLink(array $santa)
+    {
+        $participant = Participant::prepareAndSave($this->draw, $santa, $this->asymKeys['public']);
+
+        return route('dearsanta', ['santa' => Hashids::encode($participant->id)]).'#'.base64_encode($this->asymKeys['private']);
+    }
+}
+
