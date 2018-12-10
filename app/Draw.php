d49c13953476db1be62af6382f485904e544d056
Korko <jeremy.lemesle@korko.fr>
2018-12-10 23:44:55 +0100

    Split controllers and switch to base64 instead of hex

--- app/Draw.php
+++ app/Draw.php
@@ -2,9 +2,30 @@
 
 namespace App;
 
+use App\Services\SymmetricalEncrypter;
+use DateInterval;
+use DateTime;
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Draw extends Model
 {
-    //
+    public static function prepareAndSave(array $mailContent, $expiration, array $organizer, $encryptionKey)
+    {
+        $encrypter = new SymmetricalEncrypter($encryptionKey);
+
+        $date = new DateTime('now');
+        $date->add(new DateInterval('P7D'));
+
+        // Use symmetrical encryption, we don't need to hide it from each other
+        $draw = new self();
+        $draw->expiration = $expiration ?: $date->format("Y-m-d H:i:s"); // Default expiration date is 7 days
+        $draw->title = $encrypter->encrypt($mailContent['title']);
+        $draw->body = $encrypter->encrypt($mailContent['body']);
+        $draw->organizer_name = $encrypter->encrypt($organizer['name']);
+        $draw->organizer_email = $encrypter->encrypt($organizer['email']);
+        $draw->save();
+
+        return $draw;
+    }
 }
--- app/Participant.php
+++ app/Participant.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Services\AsymmetricalPublicEncrypter as AsymmetricalEncrypter;
 use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
@@ -9,4 +10,21 @@ class Participant extends Model
     const CHALLENGE = 'Ping?';
 
     public $timestamps = false;
+
+    public static function prepareAndSave(Draw $draw, array $santa, $publicEncryptionKey)
+    {
+        $encrypter = new AsymmetricalEncrypter($publicEncryptionKey);
+
+        // Use Asymmetrical encrypter, only the reciptient should be able to decrypt!
+        $participant = new self();
+        $participant->draw_id = $draw->id;
+        $participant->santa_name = $encrypter->encrypt($santa['name']);
+        $participant->santa_email = $encrypter->encrypt($santa['email']);
+        $participant->challenge = $encrypter->encrypt(Participant::CHALLENGE, false); // tested by JS so no serializing
+        $participant->public_key = $publicEncryptionKey;
+
+        $participant->save();
+
+        return $participant;
+    }
 }
