597998788cdd874cf4f5ebd806bfa4aedc4f2687
Korko <jeremy.lemesle@korko.fr>
2018-12-28 22:12:04 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/OrganizerController.php
+++ app/Http/Controllers/OrganizerController.php
@@ -19,36 +19,37 @@ class OrganizerController extends Controller
             'draw'      => $draw->id,
         ]);
     }
-/*
-    public function handle(Draw $draw, DearSantaRequest $request)
-    {
-        $santa = $this->getSanta($dearSanta, $request);
 
-        Metrics::increment('dearsanta');
-
-        $this->sendMail($santa, $request->input('title'), $request->input('content'));
-
-        $message = trans('message.sent');
-
-        return $request->ajax() ?
-            ['message' => $message] :
-            redirect('/dearsanta/'.$dearSanta->id)->with('message', $message);
-    }
-
-    private function getSanta(DearSanta $dearSanta, Request $request)
-    {
-        $key = base64_decode($request->input('key'));
-
-        $encrypter = new Encrypter($key);
-
-        return [
-            'name'  => $encrypter->decrypt($dearSanta->santa_name),
-            'email' => $encrypter->decrypt($dearSanta->santa_email),
-        ];
-    }
-
-    protected function sendMail(array $santa, $title, $content)
-    {
-        Mail::to($santa['email'], $santa['name'])->send(new DearSantaMail($title, $content));
-    }*/
+    /*
+        public function handle(Draw $draw, DearSantaRequest $request)
+        {
+            $santa = $this->getSanta($dearSanta, $request);
+    
+            Metrics::increment('dearsanta');
+    
+            $this->sendMail($santa, $request->input('title'), $request->input('content'));
+    
+            $message = trans('message.sent');
+    
+            return $request->ajax() ?
+                ['message' => $message] :
+                redirect('/dearsanta/'.$dearSanta->id)->with('message', $message);
+        }
+    
+        private function getSanta(DearSanta $dearSanta, Request $request)
+        {
+            $key = base64_decode($request->input('key'));
+    
+            $encrypter = new Encrypter($key);
+    
+            return [
+                'name'  => $encrypter->decrypt($dearSanta->santa_name),
+                'email' => $encrypter->decrypt($dearSanta->santa_email),
+            ];
+        }
+    
+        protected function sendMail(array $santa, $title, $content)
+        {
+            Mail::to($santa['email'], $santa['name'])->send(new DearSantaMail($title, $content));
+        }*/
 }
