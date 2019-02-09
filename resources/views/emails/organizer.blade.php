417444bc0ef96ed427822ba2b76d24e9f274e32f
Korko <jeremy.lemesle@korko.fr>
2019-02-09 14:10:39 +0100

    Set email design

--- resources/views/emails/organizer.blade.php
+++ resources/views/emails/organizer.blade.php
@@ -1,5 +1,5 @@
 @extends('emails/layout')
 
 @section('main')
-    {!! nl2br(htmlentities( trans('emails.organizer', ['link' => $panelLink]) )) !!}
+    {!! nl2br( trans('emails.organizer.content', ['link' => "<a href=\"$panelLink\">$panelLink</a>"]) ) !!}
 @endsection
