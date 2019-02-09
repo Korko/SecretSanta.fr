42fcb2fe5cc4599b92905971a0c4d76e430efca5
Korko <jeremy.lemesle@korko.fr>
2019-02-09 14:09:08 +0100

    Add design for organizer panel page

--- resources/views/organizer.blade.php
+++ resources/views/organizer.blade.php
@@ -11,5 +11,22 @@
 @stop
 
 @section('body')
+    <div id="form" v-cloak>
+        <div v-if="!verified" class="alert alert-danger" role="alert">
+            <span class="fas fa-exclamation-triangle" aria-hidden="true"></span>
+            <span class="sr-only">Error:</span>
+            This link is invalid, please try again
+        </div>
 
+    </div>
 @stop
+
+@section('script')
+    @parent
+
+    {!! NoCaptcha::renderJs(App::getLocale()) !!}
+
+    <script type="text/javascript" src="{{ mix('/js/common.js') }}"></script>
+    <script type="text/javascript" src="{{ mix('/js/organizer.js') }}"></script>
+@stop
+
