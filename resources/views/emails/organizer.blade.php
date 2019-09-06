cd3ae3312513230749b1d5e5de6b9a9a967122d8
Korko <jeremy.lemesle@korko.fr>
2019-09-06 13:50:42 +0200

    Add expiration date in email

--- resources/views/emails/organizer.blade.php
+++ resources/views/emails/organizer.blade.php
@@ -1,5 +1,5 @@
 @extends('emails/layout')
 
 @section('main')
-    {!! nl2br( trans('emails.organizer.content', ['link' => "<a href=\"$panelLink\">$panelLink</a>"]) ) !!}
+    {!! nl2br( trans('emails.organizer.content', ['link' => "<a href=\"$panelLink\">$panelLink</a>", 'expiration' => $draw->expires_at]) ) !!}
 @endsection
