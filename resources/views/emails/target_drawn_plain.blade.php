c7c9f6659f27c1d57755c60665d7f7dd00dc1315
Korko <jeremy.lemesle@korko.fr>
2019-11-22 19:27:50 +0100

    Add plain email content

--- resources/views/emails/target_drawn_plain.blade.php
+++ resources/views/emails/target_drawn_plain.blade.php
@@ -1,4 +1,19 @@
-{{ str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $content) }}
+Ceci est un message automatique, merci de ne pas y répondre.
 
-{{ !empty($dearSantaLink) ? trans('form.mail.post2', ['link' => $dearSantaLink]) : trans('form.mail.post') }}
+---
+
+Bonjour {{ $santa['name'] }},
+
+Vous avez été convié à un Secret Santa !
+
+Voici le message de l'organisateur :
+> {{ implode("\n> ", explode("\n", str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $content))) }}
+
+@if (!empty($dearSantaLink))
+Vous pouvez, si vous le souhaitez, écrire un petit message à votre Santa, la personne qui devra vous faire un cadeau. Pour cela, rendez-vous sur le lien suivant : {{ $dearSantaLink }}.
+
+@endif
+Amusez-vous bien !
+
+{{ config('app.name') }} - {{ config('app.url') }}
 
