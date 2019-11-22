fb93e7330ad56c3f7fcb568489fc6f9d986cf991
Korko <jeremy.lemesle@korko.fr>
2019-11-22 19:27:50 +0100

    Add plain email content

---
+++ resources/views/emails/organizer_recap_plain.blade.php
@@ -0,0 +1,10 @@
+Bonjour {{ $participants[0]['name'] }},
+
+Merci d'avoir organisé un SecretSanta en utilisant {{ config('app.name') }}. Nous espérons que vous avez apprécié le site, en cas de soucis, vous pouvez contacter le développeur à l'adresse suivante : {{ config('app.email') }}.
+
+Vous trouverez en pièce jointe un récapitulatif des participants que vous avez défini sur le site. Vous pourrez le réutiliser lors de votre prochain SecretSanta pour accélérer la rentrée des informations.
+
+Amusez-vous bien !
+
+{{ config('app.name') }} - {{ config('app.url') }}
+
