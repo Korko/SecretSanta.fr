17f1dfa2423ce7bfd14ddf2254441d9845837ca0
Korko <jeremy.lemesle@korko.fr>
2019-11-24 18:30:02 +0100

    Review emails

--- resources/views/emails/organizer_recap_plain.blade.php
+++ resources/views/emails/organizer_recap_plain.blade.php
@@ -1,8 +1,9 @@
 Bonjour {{ $participants[0]['name'] }},
 
-Merci d'avoir organisé un SecretSanta en utilisant {{ config('app.name') }}. Nous espérons que vous avez apprécié le site, en cas de soucis, vous pouvez contacter le développeur à l'adresse suivante : {{ config('app.email') }}.
+Merci d'avoir organisé un SecretSanta en utilisant {{ config('app.name') }}.
 
-Vous trouverez en pièce jointe un récapitulatif des participants que vous avez défini sur le site. Vous pourrez le réutiliser lors de votre prochain SecretSanta pour accélérer la rentrée des informations.
+Vous trouverez en pièce jointe un récapitulatif des participants que vous avez défini sur le site. Vous pourrez le réutiliser lors de votre
+prochain SecretSanta pour accélérer la rentrée des informations. En cas de problème durant votre tirage, vous pouvez aussi vérifier si il y a eu une erreur dans les données entrées.
 
 Amusez-vous bien !
 
--- resources/views/emails/target_drawn_plain.blade.php
+++ resources/views/emails/target_drawn_plain.blade.php
@@ -2,18 +2,13 @@ Ceci est un message automatique, merci de ne pas y répondre.
 
 ---
 
-Bonjour {{ $santa['name'] }},
+{{ str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $content) }}
 
-Vous avez été convié à un Secret Santa !
-
-Voici le message de l'organisateur :
-> {{ implode("\n> ", explode("\n", str_replace(['{SANTA}', '{TARGET}'], [$santa['name'], $target['name']], $content))) }}
+---
 
 @if (!empty($dearSantaLink))
-Vous pouvez, si vous le souhaitez, écrire un petit message à votre Santa, la personne qui devra vous faire un cadeau. Pour cela, rendez-vous sur le lien suivant : {{ $dearSantaLink }}.
+Pour écrire à votre Père Noël Secret : {{ $dearSantaLink }}
 
 @endif
-Amusez-vous bien !
-
 {{ config('app.name') }} - {{ config('app.url') }}
 
