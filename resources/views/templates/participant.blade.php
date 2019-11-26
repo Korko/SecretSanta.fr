e40f0346d57529268b0998c4ac5965151d1d36fe
Korko <jeremy.lemesle@korko.fr>
2019-11-26 23:59:57 +0100

    Fixes #101 - Handle exclusions via form submit

--- resources/views/templates/participant.blade.php
+++ resources/views/templates/participant.blade.php
@@ -19,7 +19,10 @@
             </div>
         </td>
         <td class="row border-right text-left participant-exclusions-wrapper">
-            <multiselect :name="'exclusions['+idx+'][]'" :options="participantNames" v-model="exclusions" track-by="value" label="text" placeholder="@lang('form.exclusions.placeholder')" :multiple="true"></multiselect>
+            <multiselect :options="participantNames" v-model="exclusions" track-by="value" label="text" placeholder="@lang('form.exclusions.placeholder')" :multiple="true"></multiselect>
+            <select style="display:none" :name="'exclusions['+idx+'][]'" multiple>
+                <option v-for="participantName in participantNames" :value="participantName.value" :selected="exclusions.find(a => (a.value === participantName.value))"></option>
+            </select>
         </td>
         <td class="row participant-remove-wrapper">
             <button type="button" class="btn btn-danger participant-remove" :disabled="participants.length <= 3" @click="$emit('delete')">
