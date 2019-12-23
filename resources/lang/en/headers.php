6f10ed63278ed8cd838dfad3ea88f023ba74eabb
Korko <jeremy.lemesle@korko.fr>
2019-12-23 16:50:22 +0100

    Remove english translation, will be retranslated later (see #8)

--- resources/lang/en/headers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'description' => "A fun organisation tool that'll help you hold your very own Secret Santa parties!",
-    'keywords'    => 'party,organisation,secretsanta,christmas,gifts,secret',
-];
