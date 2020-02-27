59aa0eb3cb727a2a1d3e81a9819bcd440742190f
Korko <jeremy.lemesle@korko.fr>
2020-02-27 14:47:38 +0100

    PHPCS

--- resources/lang/fr/dearsanta.php
+++ resources/lang/fr/dearsanta.php
@@ -5,6 +5,6 @@ return [
         'date' => "Date d'envoi",
         'body' => 'Corps du message',
         'status' => "Status de réception de l'email",
-        'empty' => "Aucun email envoyé pour le moment",
+        'empty' => 'Aucun email envoyé pour le moment',
     ],
 ];
