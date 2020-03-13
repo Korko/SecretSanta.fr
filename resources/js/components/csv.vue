b5b8dce026e0a393b6a8b3985e643e3f07c596fb
Korko <jeremy.lemesle@korko.fr>
2020-03-13 18:35:37 +0100

    Fix CSV modal

--- resources/js/components/csv.vue
+++ resources/js/components/csv.vue
@@ -1,27 +1,27 @@
 <template>
     <modal @close="$emit('close')">
         <template #header>
-            <h3>{{ lang('form.csv.title') }}</h3>
+            <h3>{{ lang.get('form.csv.title') }}</h3>
         </template>
 
         <template #body>
             <div class="alert alert-info" role="alert">
                 <span class="fas fa-question-cirle"></span>
-                <!-- {{ lang('form.csv.help', ['excel' => '<a href="https://support.office.com/fr-fr/article/Importer-ou-exporter-des-fichiers-texte-txt-ou-csv-5250ac4c-663c-47ce-937b-339e391393ba" class="alert-link">', 'calc' => '<a href="https://help.libreoffice.org/Calc/Importing_and_Exporting_CSV_Files/fr" class="alert-link">', 'elink' => '</a>']) }} -->
+                <span v-html="lang.get('form.csv.help', {excel: '<a href=\'https://support.office.com/fr-fr/article/Importer-ou-exporter-des-fichiers-texte-txt-ou-csv-5250ac4c-663c-47ce-937b-339e391393ba\' class=\'alert-link\'>', calc: '<a href=\'https://help.libreoffice.org/Calc/Importing_and_Exporting_CSV_Files/fr\' class=\'alert-link\'>', elink: '</a>'})"></span>
             </div>
 
-            {{ lang('form.csv.format') }}
+            {{ lang.get('form.csv.format') }}
             <table class="table table-bordered heavy-borders">
                 <tbody>
                     <tr>
-                        <td>{{ lang('form.csv.column1') }}</td>
-                        <td>{{ lang('form.csv.column2') }}</td>
+                        <td>{{ lang.get('form.csv.column1') }}</td>
+                        <td>{{ lang.get('form.csv.column2') }}</td>
                     </tr>
                 </tbody>
             </table>
 
             <div class="alert alert-danger" role="alert">
-                {{ lang('form.csv.warning') }}
+                {{ lang.get('form.csv.warning') }}
             </div>
 
             <form
@@ -36,11 +36,11 @@
         <template #footer>
             <button type="reset" form="uploadCsv" class="btn btn-warning">
                 <span class="fas fa-stop-circle"></span>
-                {{ lang('form.csv.cancel') }}
+                {{ lang.get('form.csv.cancel') }}
             </button>
             <button type="submit" form="uploadCsv" class="btn btn-primary">
                 <span class="fas fa-upload"></span>
-                {{ lang('form.csv.import') }}
+                {{ lang.get('form.csv.import') }}
             </button>
         </template>
     </modal>
