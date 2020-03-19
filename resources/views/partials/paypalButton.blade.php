8ae1c4cadf9c0c22e3001ab27140efc3191bf350
Korko <jeremy.lemesle@korko.fr>
2020-03-19 17:10:31 +0100

    Change Paypal integration to prevent cookie

--- resources/views/partials/paypalButton.blade.php
+++ resources/views/partials/paypalButton.blade.php
@@ -1,8 +1,5 @@
-@if (config('paypal.button_id'))
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-    <input type="hidden" name="cmd" value="_s-xclick">
-    <input type="hidden" name="hosted_button_id" value="{{ config('paypal.button_id') }}">
-    <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="@lang('form.paypal.alt')" />
-    <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-</form>
+@if (config('paypal.me'))
+<a href="https://www.paypal.me/{{ config('paypal.me') }}">
+    <img src="/images/paypal.gif">
+</a>
 @endif
