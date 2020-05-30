bcf45eb0bfccb2b3a5075f5edda671edc1afb07e
Korko <jeremy.lemesle@korko.fr>
2020-05-31 00:17:08 +0200

    Bounces improvment

---
+++ app/Services/EmailClient.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+
+use Webklex\IMAP\Facades\Client;
+
+class EmailClient
+{
+	protected $delegate;
+
+	public function __construct($connexion = 'default')
+	{
+        $this->delegate = Client::account($connexion);
+        $this->delegate->connect();
+	}
+
+    public function getUnseenMails(): Iterable
+    {
+        $oFolder = $this->delegate->getFolder('INBOX');
+
+        return $oFolder->query()->whereUnseen()->get();
+    }
+
+    /**
+     * Proxies all methods to the delegate.
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        return call_user_func_array([$this->delegate, $method], $args);
+    }
+}
