cf6cb7c51d5ec24450557d112ef61641f0ebb4f4
Korko <jeremy.lemesle@korko.fr>
2020-08-31 22:54:26 +0200

    Sign routes

--- routes/web.php
+++ routes/web.php
@@ -26,19 +26,19 @@ Route::pattern('draw:hash', '[0-9a-zA-Z]{'.config('hashids.connections.draw.leng
 Route::pattern('participant:hash', '[0-9a-zA-Z]{'.config('hashids.connections.santa.length').',}');
 
 Route::get('/dearsanta/{participant:hash}', [DearSantaController::class, 'view'])->name('dearsanta');
-Route::middleware(['decrypt.key:participant,name'])->group(function () {
+Route::middleware(['signed', 'decrypt.key:participant,name'])->group(function () {
     Route::post('/dearsanta/{participant:hash}', [DearSantaController::class, 'fetch'])->name('dearsanta.fetch');
     Route::post('/dearsanta/{participant:hash}/send', [DearSantaController::class, 'handle'])->name('dearsanta.contact');
     Route::post('/dearsanta/{participant:hash}/fetchState', [DearSantaController::class, 'fetchState'])->name('dearsanta.fetchState');
 });
 
 Route::get('/org/{draw:hash}', [OrganizerController::class, 'view'])->name('organizerPanel');
-Route::middleware(['decrypt.key:draw,mail_title'])->group(function () {
+Route::middleware(['signed', 'decrypt.key:draw,mail_title'])->group(function () {
     Route::post('/org/{draw:hash}', [OrganizerController::class, 'fetch'])->name('organizerPanel.fetch');
     Route::delete('/org/{draw:hash}', [OrganizerController::class, 'delete'])->name('organizerPanel.delete');
     Route::post('/org/{draw:hash}/fetchState', [OrganizerController::class, 'fetchState'])->name('organizerPanel.fetchState');
 });
-Route::middleware(['decrypt.key:participant,name'])->group(function () {
+Route::middleware(['signed', 'decrypt.key:participant,name'])->group(function () {
     Route::post('/org/{draw:hash}/{participant:id}/changeEmail', [OrganizerController::class, 'changeEmail'])->name('organizerPanel.changeEmail');
     Route::post('/org/{draw:hash}/{participant:id}/resendEmail', [OrganizerController::class, 'resendEmail'])->name('organizerPanel.resendEmail');
 });
