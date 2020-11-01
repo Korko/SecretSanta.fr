6c5bc9dec3be9d679d26b435e85de397f4964ca1
Korko <jeremy.lemesle@korko.fr>
2020-11-01 21:55:22 +0100

    Tiny fixes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
         <div class="item">
             <div class="polaroid">
                 <img src="/images/404-1.jpg">
-                <div class="caption">@lang('404.photo1')</div>
+                <div class="caption"></div>
             </div>
             <p class="wasted">Wasted</p>
         </div>
@@ -15,7 +15,9 @@
     <div class="error-content">
 
         <h1 class="section-title text-center">@lang('404.title')</h1>
-        <p class="lead main text-center">@lang('404.subtitle')</p>
+        <p class="lead text-center">@lang('404.subtitle')</p>
+
+        <p class="text-center"><a href="/" class="btn btn-primary">Revenir sur la page d'accueil</a></p>
 
     </div>
 @stop
