67c65df144fe21da7d43d56cf48bff6cf631d2ee
Korko <jeremy.lemesle@korko.fr>
2020-11-18 20:52:28 +0100

    Fix tests for mail draw_id

--- database/factories/MailFactory.php
+++ database/factories/MailFactory.php
@@ -3,5 +3,7 @@
 $factory->define(App\Models\Mail::class, function (Faker\Generator $faker) {
     return [
         'delivery_status' => App\Models\Mail::CREATED,
+        'draw_id' => factory(App\Models\Draw::class),
+        'version' => $faker->numberBetween(0, 255),
     ];
 });
