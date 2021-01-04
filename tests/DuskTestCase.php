a865b87426374931b696e8a844a4443b73e1209e
Korko <jeremy.lemesle@korko.fr>
2021-01-04 00:53:13 +0000

    Laravel 8.0, use Laravel Sail for Dusk

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -10,4 +10,48 @@ use Laravel\Dusk\TestCase as BaseTestCase;
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare()
+    {
+        if (! static::runningInSail()) {
+            static::startChromeDriver();
+        }
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver()
+    {
+        $options = (new ChromeOptions)->addArguments([
+            '--disable-gpu',
+            '--headless',
+            '--window-size=1920,1080',
+        ]);
+
+        return RemoteWebDriver::create(
+            $_ENV['DUSK_DRIVER_URL'] ?? 'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY, $options
+            )
+        );
+    }
+
+    /**
+     * Determine the application's base URL.
+     *
+     * @return string
+     */
+    protected function baseUrl()
+    {
+        return 'http://laravel.test';
+    }
 }
