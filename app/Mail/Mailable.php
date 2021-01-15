57a1fdf286b16cac6555c896f24903536f0f88d6
Korko <jeremy.lemesle@korko.fr>
2021-01-15 15:23:24 +0000

    Fix DKIM

--- app/Mail/Mailable.php
+++ app/Mail/Mailable.php
@@ -3,17 +3,43 @@
 namespace App\Mail;
 
 use Illuminate\Mail\Mailable as BaseMailable;
+use Swift_Signers_DKIMSigner;
 
 class Mailable extends BaseMailable
 {
     /**
-     * Send the mail and call the success method if exists
+     * Send the message using the given mailer and call the success method if exists
      *
      * @param  \Illuminate\Contracts\Mail\Factory|\Illuminate\Contracts\Mail\Mailer  $mailer
      * @return void
      */
     public function send($mailer)
     {
+        // Sign DKIM
+        $this->withSwiftMessage(function ($message) {
+            if (
+                config('mail.dkim_private_key') &&
+                file_exists(config('mail.dkim_private_key')) &&
+                config('mail.dkim_selector') &&
+                config('mail.dkim_domain')
+            ) {
+                $signer = new Swift_Signers_DKIMSigner(
+                    file_get_contents(config('mail.dkim_private_key')),
+                    config('mail.dkim_domain'),
+                    config('mail.dkim_selector'),
+                    config('mail.dkim_passphrase')
+                );
+
+                $signer->setHashAlgorithm(config('mail.dkim_algo'));
+
+                if (config('mail.dkim_identity')) {
+                    $signer->setSignerIdentity(config('mail.dkim_identity'));
+                }
+
+                $message->attachSigner($signer);
+            }
+        });
+
         parent::send($mailer);
 
         if (method_exists($this, 'success')) {
