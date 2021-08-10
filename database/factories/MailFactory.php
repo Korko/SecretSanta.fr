e75fac0f9ef9478ddeaaebafc193d2b2730d1fe6
Korko <jeremy.lemesle@korko.fr>
2021-08-10 23:59:53 +0200

    Mail factory

---
+++ database/factories/MailFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Mail as MailModel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MailFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = MailModel::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id'              => $this->faker->uuid,
+            'delivery_status' => MailModel::CREATED,
+        ];
+    }
+
+    /**
+     * Indicate that the mail has failed to be sent.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function failed()
+    {
+        return $this->state(function () {
+            return [
+                'delivery_status' => MailModel::ERROR,
+            ];
+        });
+    }
+}
