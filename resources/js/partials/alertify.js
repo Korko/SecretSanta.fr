a74d1faa05339bc929f7dfc8c7abc41be714b06c
Korko <jeremy.lemesle@korko.fr>
2021-08-11 00:15:51 +0200

    indent

--- resources/js/partials/alertify.js
+++ resources/js/partials/alertify.js
@@ -12,19 +12,19 @@ import Locale from '../vue-i18n-locales.generated.js';
 
 // Extend existing 'alert' dialog
 if (!alertify.errorAlert) {
-	//define a new errorAlert base on alert
-	alertify.dialog('errorAlert', function() {
-		return {
-			setup() {
-				return {
-					options: {
-						frameless: false,
-						movable: false,
-						closableByDimmer: false,
-						maximizable: false,
-						resizable: false
-					},
-					// Buttons and Focus copied from the source for alert dialog
+    //define a new errorAlert base on alert
+    alertify.dialog('errorAlert', function() {
+        return {
+            setup() {
+                return {
+                    options: {
+                        frameless: false,
+                        movable: false,
+                        closableByDimmer: false,
+                        maximizable: false,
+                        resizable: false
+                    },
+                    // Buttons and Focus copied from the source for alert dialog
                     buttons: [
                         {
                             text: alertify.defaults.glossary.ok,
@@ -37,16 +37,16 @@ if (!alertify.errorAlert) {
                         element: 0,
                         select: false
                     }
-				};
-			},
-			build() {
-				var errorHeader = '<span class="fa fa-times-circle fa-2x" '
-				+    'style="vertical-align:middle;color:#e10000;">'
-				+ '</span> ' + Locale[lang].common.internal;
-				this.setHeader(errorHeader);
-			}
-		};
-	}, true, 'alert');
+                };
+            },
+            build() {
+                var errorHeader = '<span class="fa fa-times-circle fa-2x" '
+                +    'style="vertical-align:middle;color:#e10000;">'
+                + '</span> ' + Locale[lang].common.internal;
+                this.setHeader(errorHeader);
+            }
+        };
+    }, true, 'alert');
 }
 
 export default alertify;
--- resources/lang/fr/error.php
+++ resources/lang/fr/error.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'validation' => 'Les données remplies ne sont pas valides',
+    'validation' => 'Les données remplies ne sont pas valides',
     'solution'   => 'Les exclusions que vous avez choisies ne permettent pas d\'attribuer une cible par participant.',
     'csrf'       => 'Le formulaire a expiré pour cause d\'inactitivé. Veuillez recommencer.',
     'signature'  => 'L\'action demandée n\'a pas pu être effectuée. Veuillez rafraichir la page avant de retenter.',
