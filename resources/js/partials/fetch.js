a33c9973d7eacb6892f17f23cf34fab06f16586a
Korko <jeremy.lemesle@korko.fr>
2021-08-11 00:14:26 +0200

    wip - use window.fetch instead of axios and use participant hash instead of id

---
+++ resources/js/partials/fetch.js
@@ -0,0 +1,17 @@
+export default function(url, method, data, headers) {
+    let body = undefined;
+    if(data) {
+        body = new FormData();
+        Object.entries(data).forEach(([k, v]) => body.append(k, v));
+    }
+
+    return window.fetch(new Request(url, {
+        method: method || 'GET',
+        headers: {
+            // Transmit Hash IV via request headers
+            'X-HASH-IV': window.location.hash.substr(1),
+            ...(headers || {})
+        },
+        body: body
+    }));
+};
