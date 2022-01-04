aec36e679cadd47ff3bde847b12acd50fb030115
Korko <jeremy.lemesle@korko.fr>
2022-01-04 19:43:03 +0100

    Remove callr config

--- config/callr.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'username' => env('CALLR_USERNAME', 'username'),
-    'password' => env('CALLR_PASSWORD', 'password'),
-    'alias' => env('CALLR_ALIAS', ''),
-    'sender' => env('CALLR_SENDER', ''),
-];
