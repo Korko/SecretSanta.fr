e9cc661e240d8788eaaad7648752d1bf29cb6e3d
Korko <jeremy.lemesle@korko.fr>
2022-08-02 20:23:58 +0200

    Fix webfonts import in mails and tests

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig(({ command, mode }) => {
         plugins: [
             laravel([
                 'resources/js/app.js',
+                'resources/sass/webfonts.scss'
             ]),
             vue({
                 template: {
