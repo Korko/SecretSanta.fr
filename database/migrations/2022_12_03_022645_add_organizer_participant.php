1541627042fd7ab3f7f360bea0347cf455793875
Korko <jeremy.lemesle@korko.fr>
2023-03-07 21:59:41 +0100

    Update migration

--- database/migrations/2022_12_03_022645_add_organizer_participant.php
+++ database/migrations/2022_12_03_022645_add_organizer_participant.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::table('draws', function (Blueprint $table) {
             $table->boolean('organizer_participant')->default(true);
 
-            // TODO: set organizer_participant to false for draws where organizer_email is different from the email of the first participant
+            // Set organizer_participant to false for draws where organizer_email is different from the email of the participant with the lowest id (organizer)
+            DB::update('UPDATE draws SET organizer_participant = 0 WHERE organizer_email != (SELECT email FROM participants WHERE draw_id = draws.id ORDER BY id ASC LIMIT 1)');
         });
     }
 
