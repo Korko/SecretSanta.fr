59aceaa9e03e03626807b570049be6a7ee821168
Korko <jeremy.lemesle@korko.fr>
2023-03-07 21:47:26 +0100

    Typehints

--- app/Casts/DrawEncryptedString.php
+++ app/Casts/DrawEncryptedString.php
@@ -5,43 +5,25 @@ namespace App\Casts;
 use App\Facades\DrawCrypt;
 use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
 
 class DrawEncryptedString implements Castable
 {
     /**
      * Get the caster class to use when casting from / to this cast target.
      *
-     * @param  array  $arguments
-     * @return object|string
+     * @param  array<string, mixed>  $arguments
      */
-    public static function castUsing(array $arguments)
+    public static function castUsing(array $arguments): CastsAttributes
     {
         return new class implements CastsAttributes
         {
-            /**
-             * Cast the given value.
-             *
-             * @param  \Illuminate\Database\Eloquent\Model  $model
-             * @param  string  $key
-             * @param  string  $value
-             * @param  array  $attributes
-             * @return mixed
-             */
-            public function get($model, $key, $value, $attributes)
+            public function get(Model $model, string $key, mixed $value, array $attributes): string
             {
                 return DrawCrypt::decrypt($value);
             }
 
-            /**
-             * Prepare the given value for storage.
-             *
-             * @param  \Illuminate\Database\Eloquent\Model  $model
-             * @param  string  $key
-             * @param  mixed  $value
-             * @param  array  $attributes
-             * @return string
-             */
-            public function set($model, $key, $value, $attributes)
+            public function set(Model $model, string $key, mixed $value, array $attributes): string
             {
                 return DrawCrypt::encrypt($value);
             }
