9f79dcdf7377b53435a3a69a9dfb41e450192a7b
Korko <jeremy.lemesle@korko.fr>
2023-03-18 00:41:51 +0100

    Try to improve Gitlab CI

---
+++ resources/views/news.blade.php
@@ -0,0 +1,13 @@
+@extends('templates/layout')
+
+@section('navbar')
+    <ul class="nav navbar-nav navbar-nav-left">
+        <li><a href="/">@lang('faq.nav.go')</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-nav-right">
+    </ul>
+@stop
+
+@section('content')
+    22
+@stop
