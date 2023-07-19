d46ff7863768b90a4d06d3480af7eceea21fb18b
Korko <jeremy.lemesle@korko.fr>
2023-07-20 00:21:21 +0200

    WIP hero

--- public/build/assets/app.61e8747b.js
+++
@@ -1,1 +0,0 @@
-Array.prototype.remove=function(){for(var t,e=arguments,h=e.length,i;h&&this.length;)for(t=e[--h];(i=this.indexOf(t))!==-1;)this.splice(i,1);return this};
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <x-slot:header>
     </x-slot:header>
 
-    <div class="w-full mx-auto bg-white flex justify-start">
+    <div class="w-full mx-auto bg-white 2xl:flex 2xl:justify-start">
         <img
             class="h-full max-h-screen -mr-[60vh] w-auto hidden 2xl:inline"
             src="{{ Vite::asset('resources/images/hero.svg') }}"
@@ -16,12 +16,12 @@
             class="2xl:hidden bg-independance"
         >
             <img
-                class="block h-full max-h-screen -mr-[60vh] w-auto"
-                src="{{ Vite::asset('resources/images/hero-full.svg') }}"
+                class="h-full max-h-screen"
+                src="{{ Vite::asset('resources/images/hero-full-flip.svg') }}"
                 alt="hero mobile"
             />
         </div>
-        <div class="flex flex-col justify-center -mt-[15%]">
+        <div class="flex flex-col justify-center 2xl:-mt-[15%]">
             <h2 class="text-melon text-4xl font-bold leading-normal">Offrez-vous des cadeaux <span class="text-desert-sand block">secrètement !</span></h2>
             <h3 class="text-2xl font-bold leading-relaxed mt-5">Qu'est-ce que Secret Santa ?</h3>
             <p class=" pr-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod tempor ornare. Mauris gravida sagittis magna convallis lacinia. Suspendisse id commodo tellus. Fusce eget hendrerit arcu. Suspendisse vehicula est non massa interdum pellentesque vitae vitae elit. Duis quis mauris eu sem convallis cursus et eget nulla. Morbi aliquam nisl elit, eget sollicitudin nunc dictum eget. Donec efficitur pellentesque malesuada. Sed euismod auctor sem sit amet malesuada.</p>
