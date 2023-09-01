3b31d1ab5cce39ddf2d0b7bb3a865d89f550685d
Korko <jeremy.lemesle@korko.fr>
2023-09-01 18:12:23 +0200

    Use Carbon instead of raw DateTime to allow fake in tests

--- app/Models/Draw.php
+++ app/Models/Draw.php
@@ -90,8 +90,8 @@ class Draw extends Model implements UrlRoutable
 
     public function prunable(): Builder
     {
-        return static::where('finished_at', '<=', (new DateTime('now'))->sub(new DateInterval('P'.self::DAYS_BEFORE_DELETION.'D')))
-            ->orWhere('updated_at', '<=', (new DateTime('now'))->sub(new DateInterval('P'.self::MONTHS_BEFORE_EXPIRATION.'M')));
+        return static::where('finished_at', '<=', Carbon::now()->subDays(self::DAYS_BEFORE_DELETION))
+            ->orWhere('updated_at', '<=', Carbon::now()->subMonths(self::MONTHS_BEFORE_EXPIRATION));
     }
 
     public function participants()
