8f07ba15c8d667ce1d1e2b02c7b18a0a44524a82
Korko <jeremy.lemesle@korko.fr>
2023-09-01 18:09:07 +0200

    Temporarly disable ReportController display

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,23 +12,12 @@ class ReportController extends Controller
 {
     public function view(Participant $participant): Response
     {
-        return response()->inertia('ReportForm', [
-            'participant' => $participant->hash,
-            'routes' => [
-                'report' => URL::signedRoute('report.report', ['participant' => $participant]),
-            ],
-        ]);
+        return '';
     }
 
     public function handle(Participant $participant, Request $request): JsonResponse
     {
-        //if($request->)
 
-        // Send report to admin
-        // => Link to remove draw and ban organizer ip
-        // => Link to write to organizer
-
-        // Withdraw participant
 
         return response()->json([
             'message' => trans('message.report_sent'),
