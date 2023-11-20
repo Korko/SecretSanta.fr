8f15a233ab3660ea2f71a8ad6861b01fdd924188
Korko <jeremy.lemesle@korko.fr>
2023-11-20 16:46:33 +0100

    Add basic database seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Draw;
+use App\Models\Participant;
+use App\Services\DrawHandler;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\URL;
+use Notification;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Notification::fake();
+
+        // Generate Draw
+        $draw = Draw::factory()
+            ->has(
+                Participant::factory()
+                    ->count(10)
+            )
+            ->create();
+
+        // Attach a random exclusion for each participant (just to spice things up)
+        $draw->participants->each(function ($participant) use ($draw) {
+            $participant->exclusions()->attach($draw->participants->except($participant->id)->random());
+        });
+
+        DrawHandler::solve($draw, $draw->participants);
+
+        $this->command->line("Organizer panel: ".URL::hashedRoute('draw.index', ['participant' => $draw->organizer]));
+    }
+}
