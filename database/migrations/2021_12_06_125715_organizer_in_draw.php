c439a322b883d9d521891a2e08528a41c1204a84
Shift <shift@laravelshift.com>
2025-12-10 10:15:49 +0000

    Adopt anonymous migrations

--- database/migrations/2021_12_06_125715_organizer_in_draw.php
+++ database/migrations/2021_12_06_125715_organizer_in_draw.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class OrganizerInDraw extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -60,4 +60,4 @@ class OrganizerInDraw extends Migration
             $table->dropColumn('organizer_email');
         });
     }
-}
+};
