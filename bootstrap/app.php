fd0a8a43de5d772e4afb581f89c00d2bf73ba1d7
Shift <shift@laravelshift.com>
2025-12-10 13:26:04 +0000

    Default new `bootstrap/app.php`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,37 +1,20 @@
 <?php
 
-use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders([
-        \Korko\Validator\ValidatorServiceProvider::class,
-    ])
+    ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn () => route('login'));
-        $middleware->redirectUsersTo(AppServiceProvider::HOME);
-
-        $middleware->alias([
-            'decrypt.iv' => \App\Http\Middleware\HandleEncryptionIV::class,
-        ]);
-
-        $middleware->priority([
-            // \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            SubstituteBindings::class,
-            // \App\Http\Middleware\EncryptCookies::class,
-            HandleEncryptionIV::class,
-            // \App\Http\Middleware\VerifyCsrfToken::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,4 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\CollectionMacroServiceProvider::class,
-    App\Providers\ComposerServiceProvider::class,
-    App\Providers\EncryptionServiceProvider::class,
-    App\Providers\ValidatorServiceProvider::class,
 ];
