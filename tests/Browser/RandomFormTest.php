d3987165bb8c5321e0f174c77494262a6745c0b6
Shift <shift@laravelshift.com>
2025-12-10 14:24:46 +0000

    Convert Dusk test classes to Pest

--- tests/Browser/RandomFormTest.php
+++ tests/Browser/RandomFormTest.php
@@ -1,60 +1,54 @@
 <?php
 
-namespace Tests\Browser;
-
 use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Components\ExclusionPicker;
 use Tests\Browser\Pages\RandomFormPage;
 use Tests\DuskTestCase;
 
-final class RandomFormTest extends DuskTestCase
-{
-    use withFaker;
+uses(Tests\DuskTestCase::class);
+uses(withFaker::class);
 
-    public function test_duplicate_name(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $name = $this->faker->name();
+test('duplicate name', function () {
+    $this->browse(function (Browser $browser) {
+        $name = $this->faker->name();
 
-            $browser->visit(new RandomFormPage)
-                ->type('participants[0][name]', $name)
-                ->type('participants[1][name]', $name)
-                ->assertPresent('input[name=\'participants[0][name]\'].is-invalid')
-                ->scrollIntoView('#randomForm button[type=submit]')
-                ->click('#randomForm button[type=submit]')
-                ->assertPresent('input[name=\'participants[1][name]\'].is-invalid');
-        });
-    }
+        $browser->visit(new RandomFormPage)
+            ->type('participants[0][name]', $name)
+            ->type('participants[1][name]', $name)
+            ->assertPresent('input[name=\'participants[0][name]\'].is-invalid')
+            ->scrollIntoView('#randomForm button[type=submit]')
+            ->click('#randomForm button[type=submit]')
+            ->assertPresent('input[name=\'participants[1][name]\'].is-invalid');
+    });
+});
 
-    public function test_no_result(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $name1 = $this->faker->name();
-            $name2 = $this->faker->name();
-            $name3 = $this->faker->name();
+test('no result', function () {
+    $this->browse(function (Browser $browser) {
+        $name1 = $this->faker->name();
+        $name2 = $this->faker->name();
+        $name3 = $this->faker->name();
 
-            $browser->visit(new RandomFormPage)
-                ->type('participants[0][name]', $name1)
-                ->type('participants[1][name]', $name2)
-                ->type('participants[2][name]', $name3)
-                ->type('participants[0][email]', $this->faker->email())
-                ->type('participants[1][email]', $this->faker->email())
-                ->type('participants[2][email]', $this->faker->email())
-                ->within(new ExclusionPicker('tr[dusk=participant0]'), function ($browser) use ($name2, $name3) {
-                    $browser->selectParticipant($name2)
-                        ->selectParticipant($name3);
-                })
-                ->within(new ExclusionPicker('tr[dusk=participant2]'), function ($browser) use ($name1) {
-                    $browser->selectParticipant($name1);
-                })
-                ->type('title', $this->faker->sentence())
-                ->type('content-email', $this->faker->paragraph())
-                ->append('content-email', '{TARGET}')
-                ->type('data-expiration', $this->faker->dateTimeBetween('+1 day', '+30 days')->format('m/d/Y'))
-                ->scrollIntoView('#randomForm button[type=submit]')
-                ->click('#randomForm button[type=submit]')
-                ->waitForText('Les exclusions que vous avez choisies ne permettent pas d\'attribuer une cible par participant.');
-        });
-    }
-}
+        $browser->visit(new RandomFormPage)
+            ->type('participants[0][name]', $name1)
+            ->type('participants[1][name]', $name2)
+            ->type('participants[2][name]', $name3)
+            ->type('participants[0][email]', $this->faker->email())
+            ->type('participants[1][email]', $this->faker->email())
+            ->type('participants[2][email]', $this->faker->email())
+            ->within(new ExclusionPicker('tr[dusk=participant0]'), function ($browser) use ($name2, $name3) {
+                $browser->selectParticipant($name2)
+                    ->selectParticipant($name3);
+            })
+            ->within(new ExclusionPicker('tr[dusk=participant2]'), function ($browser) use ($name1) {
+                $browser->selectParticipant($name1);
+            })
+            ->type('title', $this->faker->sentence())
+            ->type('content-email', $this->faker->paragraph())
+            ->append('content-email', '{TARGET}')
+            ->type('data-expiration', $this->faker->dateTimeBetween('+1 day', '+30 days')->format('m/d/Y'))
+            ->scrollIntoView('#randomForm button[type=submit]')
+            ->click('#randomForm button[type=submit]')
+            ->waitForText('Les exclusions que vous avez choisies ne permettent pas d\'attribuer une cible par participant.');
+    });
+});
