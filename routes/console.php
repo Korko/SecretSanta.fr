629d0d2c4c5d552c3bedbb29ca051e9a2ee9845e
Shift <shift@laravelshift.com>
2025-12-10 11:07:27 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -10,3 +13,21 @@
 | simple approach to interacting with each command's IO methods.
 |
 */
+
+
+Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping();
+
+Schedule::command('model:prune')
+    ->daily()
+    ->environments(['prod']);
+
+Schedule::command('secretsanta:parse-bounces')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->environments(['prod']);
+
+Schedule::command('secretsanta:clean-bounces')
+    ->daily()
+    ->environments(['prod']);
