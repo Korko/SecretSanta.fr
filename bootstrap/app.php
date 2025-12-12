55063d4f7f7dc838cfeddd88b8036a010d1565e6
Korko <jeremy.lemesle@korko.fr>
2025-12-12 01:21:52 +0100

    disable api throttle while not having an api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->throttleApi();
+        // $middleware->throttleApi();
 
         $middleware->alias([
             'decrypt.iv' => \App\Http\Middleware\HandleEncryptionIV::class,
